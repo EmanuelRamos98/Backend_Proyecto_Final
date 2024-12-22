025d1e498a0bc2bac21a1e1f4c93279d6b37d61c
EmanuelRamos98 <82544588+EmanuelRamos98@users.noreply.github.com>
2024-12-22 20:34:32 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -9,12 +9,15 @@ import contacRouter from './Routes/contac.route.js'
 import perfilRoute from './Routes/perfil.route.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { customCorsMiddleware } from './Middlewares/cors.middleware.js'
 
 
 const PORT = 3000
 const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+app.use(customCorsMiddleware)
 
 app.use(cors())
 app.use(express.json({ limit: '3mb' }))
