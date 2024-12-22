b3edbb665a829bc95b7cb5497850092f714d1d48
EmanuelRamos98 <82544588+EmanuelRamos98@users.noreply.github.com>
2024-12-22 20:51:16 -0300

    cambio link

--- src/Helpers/Email/sendValidation.email.js
+++ src/Helpers/Email/sendValidation.email.js
@@ -10,7 +10,7 @@ const sendValidationEmail = async (email, name) => {
         { expiresIn: '1d' }
     )
 
-    const redirectUrl = `http://localhost:3000/api/auth/verify-email/${validation_token}`
+    const redirectUrl = `https://backend-proyecto-final-two.vercel.app/api/auth/verify-email/${validation_token}`
     console.log(redirectUrl);
 
 
